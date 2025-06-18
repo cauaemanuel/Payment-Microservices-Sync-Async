a2681378a8deebaf98620835176a5e47a7b59b22
Caua emanuel <emanuellcaua8@gmail.com>
2025-06-18 18:51:56 -0300

    start: servico de usuario

---
+++ user-service/src/main/java/com/user_service/model/entity/User.java
@@ -0,0 +1,86 @@
+package com.user_service.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Data
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String nome;
+
+    @Column(unique = true)
+    private String email;
+
+    private String senha;
+
+    @Column(name = "USER_ROLE")
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+    public enum UserRole {
+        BUSINESS,
+        CUSTOMER;
+
+        @com.fasterxml.jackson.annotation.JsonCreator
+        public static UserRole fromString(String value) {
+            for (UserRole role : UserRole.values()) {
+                if (role.name().equalsIgnoreCase(value)) {
+                    return role;
+                }
+            }
+            throw new IllegalArgumentException("Valor inválido para userRole: " + value +
+                    ". Os valores válidos são: BUSINESS, CUSTOMER");
+        }
+
+        @com.fasterxml.jackson.annotation.JsonValue
+        public String getValue() {
+            return name();
+        }
+    }
+}
