4d91122e20aa45b38f3b5bcf4b40a03f5e5c0f44
Caua emanuel <emanuellcaua8@gmail.com>
2025-06-18 18:59:52 -0300

    feat: implementando userDetailsService

---
+++ user-service/src/main/java/com/user_service/config/security/UserDetailsServiceImple.java
@@ -0,0 +1,21 @@
+package com.user_service.config.security;
+
+import com.user_service.model.entity.User;
+import com.user_service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImple implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com o email: " + username));
+        return user;
+    }
+}
