fc0504468c3f4d3726ecfe7d8c0435cb27b9e225
Caua emanuel <emanuellcaua8@gmail.com>
2025-06-20 21:39:50 -0300

    init: iniciando process api payment

---
+++ payment-processor-service/src/main/java/com/payment_processor_service/PaymentProcessorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.payment_processor_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentProcessorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentProcessorServiceApplication.class, args);
+	}
+
+}
